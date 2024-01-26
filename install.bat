78e7a39bfdd49a230abc99d68e6b1046a3a675c5
Wick Studio <53820568+wickstudio@users.noreply.github.com>
2024-01-26 22:20:06 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@ echo Installing necessary Python packages for the cleanup tool...
 pip install pyfiglet
 pip install winshell
 pip install colorama
+pip install pywin32
 
 echo Installation completed.
 pause
